a28072dfbee96c0e338c2b87133a531bffcf807f
Ramon Silva Barros <ramon@dcfdci.fdci.br>
2024-07-15 13:19:55 -0300

    configurando o setup para desenvolvimento

--- src/styles/index.ts
+++ src/styles/index.ts
@@ -1,4 +1,5 @@
-import { createGlobalStyle } from 'styled-components'
+import styled, { createGlobalStyle } from 'styled-components'
+
 const EstiloGlobal = createGlobalStyle`
   * {
     margin: 0;
@@ -7,4 +8,10 @@ const EstiloGlobal = createGlobalStyle`
     font-family: Roboto, sans-serif;
   }
 `
+
+export const Container = styled.div`
+  display: grid;
+  grid-template-columns: 224px auto;
+`
+
 export default EstiloGlobal
